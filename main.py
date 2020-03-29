1c26c065fa7872122d95ea3363547f0aa78c9706
jai shiva <jai.shiva4@gmail.com>
2020-03-29 21:14:34 +0530

    add scoring logic and display

--- main.py
+++ main.py
@@ -35,21 +35,31 @@ dy = 2
 # paddle up
 def paddle_up_a():
     if paddle_a.ycor() < 250:
-        paddle_a.sety(paddle_a.ycor()+10)
+        paddle_a.sety(paddle_a.ycor()+20)
 
 def paddle_up_b():
     if paddle_b.ycor() < 250:
-        paddle_b.sety(paddle_b.ycor()+10)
+        paddle_b.sety(paddle_b.ycor()+20)
 
 # paddle down
 def paddle_down_a():
     if paddle_a.ycor() > -250:
-        paddle_a.sety(paddle_a.ycor()-10)
+        paddle_a.sety(paddle_a.ycor()-20)
 
 def paddle_down_b():
     if paddle_b.ycor() > -250:
-        paddle_b.sety(paddle_b.ycor()-10)
+        paddle_b.sety(paddle_b.ycor()-20)
 
+
+# score
+score_a = 0
+score_b = 0
+pen = turtle.Turtle()
+pen.goto(0,270)
+pen.hideturtle()
+pen.clear()
+pen.color('white')
+pen.write('Player A {}  Player B {}'.format(score_a,score_b),False, align='center',font=('Courier',24))
 # listen for key strokes
 window.listen()
 window.onkeypress(paddle_up_a, 'w')
@@ -60,10 +70,21 @@ window.onkeypress(paddle_down_b, 'Down')
 
 while True:
     ball.setpos(ball.xcor()+dx,ball.ycor()+dy)
+
     if ball.ycor()>290 or ball.ycor() < -290:
         dy = -dy
     if ball.ycor() > paddle_a.ycor()-50 and ball.ycor() < paddle_a.ycor()+50 and ball.xcor() == paddle_a.xcor()+10:
         dx = -dx
     if ball.ycor() > paddle_b.ycor()-50 and ball.ycor() < paddle_b.ycor()+50 and ball.xcor() == paddle_b.xcor()-10:
         dx = -dx
+    if ball.xcor() > 400:
+        score_a += 1
+        ball.setpos(0,0)
+        pen.clear()
+        pen.write('Player A {}  Player B {}'.format(score_a,score_b),False, align='center',font=('Courier',24))
+    if ball.xcor() < -400:
+        score_b += 1
+        ball.setpos(0,0)
+        pen.clear()
+        pen.write('Player A {}  Player B {}'.format(score_a,score_b),False, align='center',font=('Courier',24))
     window.update()
